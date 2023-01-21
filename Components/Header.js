7729ab73c38459bed98be8f875fa287b6e6fd7f3
madhavi566 <madhavibogani@gmail.com>
2023-01-21 10:06:08 +0530

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -1,17 +1,37 @@
+import Logo from '../assets/img/food_love.jpeg'
+import {useState} from 'react';
 
-const Title = <a href="/"> <b className="title">Vintage</b> <img className="food" src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyOiu6Yh_P14AlkIVM9L2syu7AVNOAcVHiqg&usqp=CAU" alt="header"/></a>
+const Title  = () => {
+return (
+<>
+<a href="/"> 
+<img className="logo" 
+src = {Logo} alt="logo"/></a>
+</>
+)}
+
+
+const loggedInUser = () => {
+    return true;
+}
 
 const Header = () =>{
+    const [loggedIn,SetLoggedIn] = useState(false);
+    const [title,setTitle] = useState("Food Villa");
     return ( 
-    <div className="header">
-      {Title}
+    <div className="header"> 
+       {Title()}
+       <h1>{title}</h1>
+        <button onClick = {()=>{setTitle("change the title")}}>Change Title </button>
        <div className="nav-items">
       <ul>
           <li>Home</li>
           <li>About</li>
           <li>Support</li>
           <li>Cart</li>
-       </ul>   
+       </ul>
+{loggedIn ? <button onClick = {()=> {SetLoggedIn(false)}}>Logout </button> :<button onClick = {()=> {SetLoggedIn(true)}}>Login </button> } 
+
    </div>
     </div>
    )
