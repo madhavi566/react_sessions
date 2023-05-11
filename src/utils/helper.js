fc43373c805464608eb0df78b8f93ff9eeba6c48
madhavi566 <madhavibogani@gmail.com>
2023-05-11 17:08:11 +0530

    Delete src

--- src/utils/helper.js
+++
@@ -1,4 +0,0 @@
-export function filterData(searchText,restaurants) {
-    const filteredRestaurants =  restaurants.filter((item)=> item?.data?.name.toLowerCase().includes(searchText.toLowerCase()))
-    return  filteredRestaurants;
-  }
